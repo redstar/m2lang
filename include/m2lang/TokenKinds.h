974359ef8f345a3bc374ebe7341eec08c1e5259e
Kai Nacke <kai@redstar.de>
2019-02-08 07:22:26 +0100

    Begin work on hand-coded parser and lexer.
    Define the token classes first.

---
+++ include/m2lang/TokenKinds.h
@@ -0,0 +1,35 @@
+//===--- TokenKinds.h - Enum values for Modula-2 Token Kinds ----*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the BSD 3-Clause License.
+// See the LICENSE file for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the m2lang::TokenKind enum and support functions.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef M2LANG_TOKENKINDS_H
+#define M2LANG_TOKENKINDS_H
+
+#include "llvm/Support/Compiler.h"
+
+namespace m2lang {
+
+  namespace tok {
+    enum TokenKind : unsigned short {
+#define TOK(X) X,
+#include "TokenKinds.def"
+      NUM_TOKENS
+    };
+
+    const char *getTokenName(TokenKind Kind) LLVM_READNONE;
+
+    const char *getPunctuatorSpelling(TokenKind Kind) LLVM_READNONE;
+
+    const char *getKeywordSpelling(TokenKind Kind) LLVM_READNONE;
+  }  // end namespace tok
+}  // end namespace m2lang
+
+#endif
---
+++ lib/TokenKinds.cpp
@@ -0,0 +1,48 @@
+//===--- TokenKinds.cpp - Token Kinds Support -------------------*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the BSD 3-Clause License.
+// See the LICENSE file for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Implements the TokenKind enum and support functions.
+///
+//===----------------------------------------------------------------------===//
+
+#include "m2lang/TokenKinds.h"
+#include "llvm/Support/ErrorHandling.h"
+
+using namespace m2lang;
+
+static const char * const TokNames[] = {
+#define TOK(X) #X,
+#define KEYWORD(X,Y) #X,
+#include "m2lang/TokenKinds.def"
+  nullptr
+};
+
+const char *tok::getTokenName(TokenKind Kind) {
+  if (Kind < tok::NUM_TOKENS)
+    return TokNames[Kind];
+  llvm_unreachable("unknown TokenKind");
+  return nullptr;
+}
+
+const char *tok::getPunctuatorSpelling(TokenKind Kind) {
+  switch (Kind) {
+#define PUNCTUATOR(X,Y) case X: return Y;
+#include "m2lang/TokenKinds.def"
+    default: break;
+  }
+  return nullptr;
+}
+
+const char *tok::getKeywordSpelling(TokenKind Kind) {
+  switch (Kind) {
+#define KEYWORD(X,Y) case kw_ ## X: return #X;
+#include "m2lang/TokenKinds.def"
+    default: break;
+  }
+  return nullptr;
+}
