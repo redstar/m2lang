f8f1b0909dbccd7ab1457907f76fb7eddd2bf720
Kai Nacke <kai@redstar.de>
2019-08-13 07:34:31 +0200

    Add more header files.
    - Preprocessor.h: Definition of the preprocesor
    - TargetOptions.h: Collection of options from command line

---
+++ include/m2lang/Basic/TargetOptions.h
@@ -0,0 +1,47 @@
+//===--- TargetOptions.h - M2 Language Family Target Options ----*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the m2lang::TargetOptions class.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef M2LANG_BASIC_TARGETOPTIONS_H
+#define M2LANG_BASIC_TARGETOPTIONS_H
+
+namespace m2lang {
+/// Options for controlling the target.
+class TargetOptions {
+public:
+  /// The name of the target triple to compile for.
+  std::string Triple;
+
+  /// When compiling for the device side, contains the triple used to compile
+  /// for the host.
+  std::string HostTriple;
+
+  /// If given, the name of the target CPU to generate code for.
+  std::string CPU;
+
+  /// The list of target specific features to enable or disable, as written on the command line.
+  std::vector<std::string> FeaturesAsWritten;
+
+  /// The list of target specific features to enable or disable -- this should
+  /// be a list of strings starting with by '+' or '-'.
+  std::vector<std::string> Features;
+
+  // The code model to be used as specified by the user. Corresponds to
+  // CodeModel::Model enum defined in include/llvm/Support/CodeGen.h, plus
+  // "default" for the case when the user has not explicitly specified a
+  // code model.
+  std::string CodeModel;
+};
+
+} // namespace m2lang
+
+#endif
---
+++ include/m2lang/Lexer/Preprocessor.h
@@ -0,0 +1,24 @@
+//===--- Preprocessor.h - Modula-2 Language Preprocessor --------*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the Preprocessor interface.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef M2LANG_LEXER_PREPROCESSOR_H
+#define M2LANG_LEXER_PREPROCESSOR_H
+
+namespace m2lang {
+
+class Preprocessor {
+};
+
+} // end namespace m2lang
+
+#endif
