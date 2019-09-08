27990764eb324576d64396e253b43e3baf70767e
Kai Nacke <kai@redstar.de>
2019-09-08 14:14:09 +0200

    Add very simple diagnostic engine.
    This is only to be able to output some messages. Needs more refinement!

---
+++ include/m2lang/Basic/Diagnostic.h
@@ -0,0 +1,44 @@
+//===--- Diagnostic.h - M2 Language Family Diagnostic Handling --*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the Diagnostic-related interfaces.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef M2LANG_BASIC_DIAGNOSTC_H
+#define M2LANG_BASIC_DIAGNOSTC_H
+
+//#include "m2lang/Basic/SourceLocation.h"
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+
+namespace m2lang {
+
+namespace diag {
+  enum {
+#define DIAG(X,Y) X,
+#include "m2lang/Basic/Diagnostic.def"
+#undef DIAG
+  };
+}
+
+class DiagnosticsEngine : public llvm::RefCountedBase<DiagnosticsEngine> {
+
+public:
+  explicit DiagnosticsEngine();
+  DiagnosticsEngine(const DiagnosticsEngine &) = delete;
+  DiagnosticsEngine &operator=(const DiagnosticsEngine &) = delete;
+  ~DiagnosticsEngine();
+
+  //void Report(SourceLocation Loc, unsigned DiagID);
+  void Report(unsigned DiagID);
+};
+
+} // namespace m2lang
+
+#endif
---
+++ lib/Basic/Diagnostic.cpp
@@ -0,0 +1,37 @@
+//===-- Diagnostic.cpp - M2 Language Family Diagnostic Handling -*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+//  Implements the Diagnostic-related interfaces.
+///
+//===----------------------------------------------------------------------===//
+
+#include "m2lang/Basic/Diagnostic.h"
+#include "llvm/Support/raw_os_ostream.h"
+
+using namespace m2lang;
+
+namespace {
+  const char* DiagnosticText[] = {
+#define DIAG(X,Y) Y,
+#include "m2lang/Basic/Diagnostic.def"
+#undef DIAG
+  };
+  const char* GetDiagnosticText(unsigned DiagID) {
+    return DiagnosticText[DiagID];
+  }
+
+}
+
+DiagnosticsEngine::DiagnosticsEngine() {
+
+}
+
+void DiagnosticsEngine::Report(unsigned DiagID) {
+  llvm::errs() << GetDiagnosticText(DiagID) << "\n";
+}
