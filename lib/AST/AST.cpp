423cacf465b4b7adaf4e868caf2eef5de719f61f
Kai Nacke <kai@redstar.de>
2019-09-28 21:23:15 +0200

    Add empty AST files.
    Creates the directory structure for AST implementation.

---
+++ lib/AST/AST.cpp
@@ -0,0 +1,12 @@
+//===--- AST.cpp - Modula-2 Abstract Syntax Tree ----------------*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the AST implementation.
+///
+//===----------------------------------------------------------------------===//
