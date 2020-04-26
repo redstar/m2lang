2bed2a60301a9ff628df406187d17b6f0507cc2e
Kai Nacke <kai@redstar.de>
2020-04-26 14:01:07 +0200

    Add CodeGen library to build.

---
+++ lib/CodeGen/CodeGen.cpp
@@ -0,0 +1,12 @@
+//===--- CodeGen.cpp - Modula-2 Language Code Generator ---------*- C++ -*-===//
+//
+// Part of the M2Lang Project, under the Apache License v2.0 with
+// LLVM Exceptions. See LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// Defines the CodeGen implementation.
+///
+//===----------------------------------------------------------------------===//
