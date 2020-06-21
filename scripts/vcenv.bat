559cbc1157738afa7ef9c7c238e2bbed3a5aa106
Kai Nacke <kai@redstar.de>
2020-06-21 13:39:12 +0200

    Travis/Windows: Install and use VS2019.
    This also requires an update to vcenc.bat. Linking still fails
    because of a build mode mismatch.

--- scripts/vcenv.bat
+++ scripts/vcenv.bat
@@ -1,5 +1,12 @@
 @echo off
 if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 > nul
+        echo # USING VISUAL STUDIO 16
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" amd64
         echo # USING VISUAL STUDIO 15
